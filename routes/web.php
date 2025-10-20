fa21a63016b87176d36d180dc5f8c4b249300037
Sourov Biswas <sourovcodes@gmail.com>
2025-10-20 19:20:27 +0600

    feat: implement Google authentication with login and logout functionality

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,8 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('home');
 })->name('home');
+
+Route::get('/login', [\App\Http\Controllers\Auth\AuthController::class, 'login'])->name('login')->middleware(['guest']);
+Route::post('/logout', [\App\Http\Controllers\Auth\AuthController::class, 'logout'])->name('logout')->middleware(['auth']);
+Route::get('/auth/google', [\App\Http\Controllers\Auth\GoogleAuthController::class, 'redirect'])->name('auth.google')->middleware(['guest']);
+Route::get('/auth/google/callback', [\App\Http\Controllers\Auth\GoogleAuthController::class, 'callback'])->name('auth.google.callback')->middleware(['guest']);
